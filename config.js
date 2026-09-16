a36a7a820b65c819b4de463aa61a1858736ca4fd
ccarasis <christinacarasis@gmail.com>
2026-09-15 20:28:59 -0700

    Update map configuration and chapter details
    Updated the map style, title, and footer. Modified chapter details including locations and descriptions.

--- config.js
+++ config.js
@@ -1,91 +1,27 @@
 var config = {
-    // style: 'mapbox://styles/mapbox/satellite-v9',
-    // leave commented to use Mapbox Standard Style
+    style: 'mapbox://styles/mapbox/satellite-streets-v12',
     accessToken: 'pk.eyJ1IjoiY2NhcmFzaXMiLCJhIjoiY210dnp4dHBlMDRsNDJ5b2FzbzMwYzd0NCJ9.jjPwOMlmB3aCwKp8JpKS_Q',
     showMarkers: true,
     markerColor: '#8db855',
-    //projection: 'equirectangular',
-    //Read more about available projections here
-    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
-    inset: true,
-    insetOptions: {
-        markerColor: 'orange'
-    },
-    insetPosition: 'bottom-right',
     theme: 'dark',
-    use3dTerrain: true, //set true for enabling 3D maps.
-    auto: false,
-    title: 'Arvanitiochoria',
+    use3dTerrain: true,
+    title: 'Arvanitochoria',
     subtitle: 'Arvanite Cartographies of Mt. Arachnaio',
     byline: 'By Christina Maria Carasis <br> University of California, Berkeley <br> Department of Geography',
-    footer: 'Sources: source citations, etc.'
+    footer: 'Sources: Department of Geography, UC Berkeley',
+    chapters: [
         {
-            id: 'slug-style-id',
+            id: 'introduction',
             alignment: 'left',
             hidden: false,
             title: 'What are the Arvanitochoria?',
-            image: './assets/san-fran.jpeg',
-            description: 'The first chapter contains a title, image, and camera view for San Francisco, California. Update the chapter data to make it your own.',
+            image: '',
+            description: 'The first chapter contains an introduction to Arvanite cultural sites around Mt. Arachnaio.',
             location: {
-                center: [37.6793, 22.9562],
-                zoom: 8.5,
+                center: [22.9562, 37.6793],
+                zoom: 11,
                 pitch: 60,
                 bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: true,
-            callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
-        },
-        {
-            id: 'second-identifier',
-            alignment: 'right',
-            hidden: false,
-            title: 'Washington, D.C.',
-            image: './assets/washington-dc.jpg',
-            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
-            location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: -43.2,
-                // flyTo additional controls-
-                // These options control the flight curve, making it move
-                // slowly and zoom out almost completely before starting
-                // to pan.
-                //speed: 2, // make the flying slow
-                //curve: 1, // change the speed at which it zooms out
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: true,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'third-identifier',
-            alignment: 'left',
-            hidden: false,
-            title: 'Geneva',
-            image: './assets/geneva.jpg',
-            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains. Known as a global hub for diplomacy and finance, it is home to numerous international organizations, including the United Nations and the Red Cross.',
-            location: {
-                center: [6.15116, 46.20595],
-                zoom: 12.52,
-                pitch: 8.01,
-                bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -94,17 +30,17 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'fourth-chapter',
-            alignment: 'fully',
+            id: 'agia-triada',
+            alignment: 'right',
             hidden: false,
-            title: 'Buenos Aires',
-            image: './assets/buenos-aires.jpg',
-            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            title: 'Church of Agia Triada (Limnes)',
+            image: '',
+            description: 'Holy Trinity Church in Limnes village square.',
             location: {
-                center: [-58.54195, -34.71600],
-                zoom: 4,
-                pitch: 0,
-                bearing: 0
+                center: [22.8804, 37.7128],
+                zoom: 16.5,
+                pitch: 65,
+                bearing: -10
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
