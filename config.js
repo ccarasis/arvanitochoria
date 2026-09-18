6f395504a9e4cbd864f3134d40fe2600e514a91e
ccarasis <christinacarasis@gmail.com>
2026-09-17 21:21:16 -0700

    Fix zoom level in config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var config = {
             description: 'Introduction to Arvanite villages near Mt. Arachnaio.',
             location: {
                 center: [22.9693, 37.6407],
-                zoom: 12,
+                zoom: 07,
                 pitch: 60,
                 bearing: 130
             }
