35a75342ef309c8f44116271097cc188f7c5e6c0
ccarasis <christinacarasis@gmail.com>
2026-09-17 21:11:05 -0700

    Increase zoom levels for map locations

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var config = {
             description: 'Introduction to Arvanite villages near Mt. Arachnaio.',
             location: {
                 center: [22.9693, 37.6407],
-                zoom: 15,
+                zoom: 17,
                 pitch: 50,
                 bearing: 130
             }
@@ -32,7 +32,7 @@ var config = {
             description: 'The Holy Trinity Church in the village square of Limnes.',
             location: {
                 center: [22.8802, 37.7141],
-                zoom: 15,
+                zoom: 18,
                 pitch: 60,
                 bearing: -10
             }
