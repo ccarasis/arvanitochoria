f0db69686bc7f447a4e352724911688f57993838
ccarasis <christinacarasis@gmail.com>
2026-09-20 11:50:24 -0700

    Adjust map zoom and pitch settings
    Updated map configuration to enhance zoom and pitch.

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ var config = {
             description: 'Placeholder text about Arvanite settlements on Mt. Arachnaio.',
             location: {
                 center: [22.91, 37.68],
-                zoom: 13.5,
-                pitch: 65,
+                zoom: 15,
+                pitch: 75,
                 bearing: 130
             }
         },
