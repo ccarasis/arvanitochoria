99a75078d4de7b94061f25acc94ed862af4b5738
ccarasis <christinacarasis@gmail.com>
2026-09-21 12:51:19 -0700

    Change map center coordinates
    Updated the center coordinates for the map location.

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ var config = {
             title: 'What are the Arvanitochoria?',
             description: 'Placeholder text about Arvanite settlements on Mt. Arachnaio.',
             location: {
-                center: [22.96, 37.69],
+                center: [22.99, 37.71],
                 zoom: 12,
                 pitch: 55,
                 bearing: 0
